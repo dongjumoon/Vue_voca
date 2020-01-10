c48aa8f12c52df6d7a36205eab1d77ae06ab4dbf
dongjumoon <dongju.moon@mackerly.com>
2020-01-10 17:34:09 +0900

    퀴즈풀기&단어추가 페이지 HTML,CSS작업(+임시로. 데이터 반응 틀 작성)

--- src/components/quiz_component.vue
+++ src/components/quiz_component.vue
@@ -1,11 +1,47 @@
 <template>
     <div>
-
+        <header class="header">
+            <h1 class="page-title">{{ pageTitle }}</h1>
+            <a href="/"
+               title="메인화면으로 이동"
+               class="go-back-btn">
+                <span>Back</span>
+            </a>
+        </header>
+        <section>
+            <div>
+                <p class="question">
+                    {{ question }}
+                    <span class="question-num">{{ questionNum }}</span>
+                </p>
+            </div>
+            <div>
+                <input v-model="answer" class="answer" type="text" placeholder="정답 입력">
+                <button class="submit-btn" v-on:click="resultView">제출하기</button>
+            </div>
+        </section>
+        <!-- 모달팝업 추가(오답,정답알림) -->
     </div>
 </template>
 
 <script>
     export default {
+        data: function() {
+            return {
+                pageTitle: '퀴즈 풀기',
+                answer: '',
+                question: '설명설명'
+            }
+        },
+        computed: {
+            'questionNum' : function() {
+               return '1/10';
+            }
+        },
+        methods: {
+            resultView: function() {
+            }
+        }
     }
 </script>
 
