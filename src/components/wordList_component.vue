18a497029eee7fa6ec237bfb56b0c315bbcbdd8e
dongjumoon <dongju.moon@mackerly.com>
2020-01-10 19:57:02 +0900

    단어목록 페이지 HTML,CSS작업

--- src/components/wordList_component.vue
+++ src/components/wordList_component.vue
@@ -1,11 +1,37 @@
 <template>
     <div>
+        <header class="header">
+            <h1 class="page-title">{{ pageTitle }}</h1>
+            <a href="/"
+               title="메인화면으로 이동"
+               class="go-back-btn">
+                <span>Back</span>
+            </a>
+        </header>
+        <div class="word-list-box">
+            <div class="toggle-btn-area">
+                <button class="viewer-toggle">Page / scroll</button>
+            </div>
+            <section>
+                <ul class="word-list">
+                    <li v-for="(word, index) in words" v-bind:key="word">{{ (index + 1) + '.' }} {{ word }}</li>
+                </ul>
+                <nav>
 
+                </nav>
+            </section>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
+        data: function() {
+            return {
+                pageTitle: '단어 목록보기',
+                words: ['red', 'blue', 'component', 'function', 'method']
+            }
+        }
     }
 </script>
 
